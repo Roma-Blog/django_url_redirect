ca1664adb75e9003a8dd3ac808b1e60b7740b300
Roman <ryzikov16@gmail.com>
2024-04-16 21:35:19 +0700

    first commit

---
+++ link_shortening/redirect_links/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+import random
+
+char = ['a','b','c','d','f','g','h','i','j',
+        'k','l','m','n','o','p','q','r','s',
+        't','u','v','w','x','y','z','1','2',
+        '3','4','5','6','7','8','9','0']
+
+
+# def generate_short_link() -> str:
+#     not_unique = True
+#     while not_unique:
+#         unique_ref = ''.join(random.choices(char, k=10))
+#         if not Links.objects.filter(short_link = unique_ref):
+#             not_unique = False
+#     return unique_ref
+
+class Links(models.Model):
+
+    link = models.CharField(max_length=500)
+    short_link = models.CharField(max_length=64, unique=True)
+
+
