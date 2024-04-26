77bb9702ecbd06ef69839c7e1278b6162b30cfd7
Roman <ryzikov16@gmail.com>
2024-04-26 22:20:06 +0700

    0.066

--- link_shortening/redirect_links/views.py
+++ link_shortening/redirect_links/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import redirect, render
 from django.http import Http404
 from .forms import CompaniesForm
 from .models import Companies, Sesions
+from django.db.models import Avg, Max, Min, Sum
 import random, requests, datetime
 
 def generate_short_link() -> str:
@@ -43,6 +44,25 @@ def redirect_link(request, short_link):
         city = get_city(ip_user)
     )
 
+    # Генерация 1 000 000 сессий
+    # date_now = datetime.datetime.now().date()
+    # count_sesions = random.randint(1, 50) 
+    # day = 0
+    # for i in range(1000000):
+    #     if count_sesions == 0:
+    #         count_sesions = random.randint(1, 50) 
+    #         day += 1
+
+    #     Sesions.objects.create(
+    #         id_company = company,
+    #         ip_user = '127.{a}.{b}.{c}'.format(a = random.randint(0, 255), b = random.randint(0, 255), c = random.randint(0, 255)),
+    #         browser = random.choice(['Chrome', 'Firefox', 'Safari', 'Opera', 'Edge']),
+    #         city = random.choice(['Москва', 'Новосибирск','Барнаул','Иркутск','Владивосток','Тюмень','Кемерово','Санкт-Петербург']),
+    #         data = date_now + datetime.timedelta(days = day)
+    #     )
+    #     count_sesions -= 1
+
+
     return redirect(company.link, permanent=True)
 
 def list_companies(request):
@@ -69,30 +89,46 @@ def add_company(request):
     return render(request, 'redirect_links/add_company.html', context)
 
 def detail_company(request, id):
-    data_beginning = datetime.date.today() - datetime.timedelta(days=7)
-    data_end = datetime.date.today()
+    data_beginning = None
+    data_end = None
 
     if request.method == 'GET':
         if request.GET.get('beginning') and request.GET.get('end'):
-            data_beginning = request.GET.get('beginning')
-            data_end = request.GET.get('end')
+            
+            data_beginning = datetime.datetime(*list(map(int, request.GET.get('beginning').split('-')))).date()
+            data_end = datetime.datetime(*list(map(int, request.GET.get('end').split('-')))).date()
     
-    count_days = (data_end - data_beginning).days
     sesions = Sesions.objects.filter(id_company = id)
     company = Companies.objects.get(id = id)
     list_data = list(item.data for item in sesions)
     sesions_data: str = ''
 
-    for i in range(1, count_days + 1):
+    if not data_beginning and not data_end:
+        data_beginning = list_data[0] - datetime.timedelta(days=6)
+        data_end = list_data[0]
+
+    count_days = (data_end - data_beginning).days
+
+    for i in range(count_days + 1):
         if data_beginning + datetime.timedelta(days=i) in list_data:
             sesions_data = sesions_data + '{' + f'x: "{data_beginning + datetime.timedelta(days=i)}", y: {list_data.count(data_beginning + datetime.timedelta(days=i))}' + '},'
         else:
             sesions_data = sesions_data + '{' + f'x: "{data_beginning + datetime.timedelta(days=i)}", y: 0' + '},'
         
+    list_sesions_date = []
+
+    for item in sesions:
+        if item.data >= data_beginning and item.data <= data_end:
+            list_sesions_date.append(item)
+
     context = {
         'company': company,
-        'sesions': sesions,
-        'sesions_data': sesions_data
+        'sesions': list_sesions_date,
+        'sesions_data': sesions_data,
+        'data_beginning': str(data_beginning),
+        'data_end': str(data_end),
+        'min_data': str(list_data[-1]),
+        'max_data': str(list_data[0]),
     }
     return render(request, 'redirect_links/detail_company.html', context)
 
